e68acab28f6672258ca46152dd5b1104cc0a3552
MALWICK <batahumphrey66@gmail.com>
2023-04-02 15:14:56 +0100

    creating my quiz  user interface

---
+++ app.js
@@ -0,0 +1,40 @@
+const questions = [
+  {
+    question: "how old is Humphrey?",
+    a: "10",
+    b: "17",
+    c: "20",
+    d: "26",
+    correct: "c",
+  },
+  {
+    question: "how is the most used programming language in 2023",
+    a: "javascript",
+    b: "java",
+    c: "python",
+    d: "c",
+    correct: "a",
+  },{
+    question: "how is the president of Cameroon",
+    a: "Joe Bidone",
+    b: "Poul Biya",
+    c: "Tinibu Orashus",
+    d: "Poul kagame",
+    correct: "b",
+  },{
+    question: "what does HTML stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminal Motorboats Lamborgini", 
+    correct: "a"
+  },
+  {
+    question: "what year was Javascript Launched?",
+    a: "1996",
+    b: "1994",
+    c: "1993",
+    d: "None of the above",
+    correct: "d"
+  },
+];
